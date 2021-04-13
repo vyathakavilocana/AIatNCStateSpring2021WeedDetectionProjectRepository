392a7e370418a2f546420bd3f9037107efbffe00
vyathakavilocana <prathammc@gmail.com>
2021-04-13 01:00:59 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Spring 2021 AI at NC State project repository contains all the code for the Weed Detection project. Utilizing images and other geospatial data, it [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D machine learning prototyping code for the Weed Detection project. Utilizing images and other geospatial data, it seeks to harness the power of applied AI models to sense where weeds are amongst g[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[distinguish weeds from [D[D[D[D[D[D endemic to North Carolina from the grasses occupying the terrain of the state as well as provide alternative productive uses for the weeds (foraging, etc).',
+    author='AI at NC State (Pratham Chhabria, Yuseung Kim, Jiale Hu, Sumitosh Pal, Prachi Jadhav, Jared Arriola) and Clemson AI Club (Jeffrey Wang)',
+    license='MIT',
+)
